b00e76bce1dd0bcb70965b5ad839dc09557c044b
Felipe Coury <felipe.coury@gmail.com>
2026-02-28 11:27:17 -0300

    chore: removed web

--- web/src/modules/setup/setup-page.tsx
+++
@@ -1,5 +0,0 @@
-import { SetupWizard } from "./setup-wizard";
-
-export function SetupPage(): React.ReactElement {
-  return <SetupWizard />;
-}
--- web/src/modules/workspaces/workspaces-utils.ts
+++
@@ -1,9 +0,0 @@
-export function parentPath(path: string): string {
-  const trimmed = path.trim().replace(/^\/+|\/+$/g, "");
-  if (!trimmed) {
-    return "";
-  }
-  const parts = trimmed.split("/");
-  parts.pop();
-  return parts.join("/");
-}
